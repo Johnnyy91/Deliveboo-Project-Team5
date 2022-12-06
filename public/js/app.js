157521242dcdba5a01c8cc69b0d2d88e6dc970c3
Johnnyy91 <claudiosardilli@gmail.com>
2022-12-06 11:59:44 +0100

    delete cross-env

--- public/js/app.js
+++ public/js/app.js
@@ -49729,8 +49729,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\MAMP\htdocs\Deliveboo-Classe71-Team5\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\MAMP\htdocs\Deliveboo-Classe71-Team5\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Users/johnnyyy/Desktop/ESERCITAZIONI_BOOLEAN/Deliveboo-Classe71-Team5/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Users/johnnyyy/Desktop/ESERCITAZIONI_BOOLEAN/Deliveboo-Classe71-Team5/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
