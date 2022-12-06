bd0555f9c3754a5473c5b46f4952a5a12eae298d
ErosEros <eros.vittori1994@gmail.com>
2022-12-06 17:08:15 +0100

    add view to guest home

---
+++ resources/js/views/App.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>App vue</div>
+  </template>
+
+  <script>
+  export default {
+      name:'App',
+  }
+  </script>
+
+  <style>
+  </style>
--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -1,1 +1,23 @@
-@extends('layouts.app') <!--estendo layout.app-->
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('css/app.css')}}">
+    <script src="{{ asset('js/front.js')}}" defer></script>
+    <title>Home</title>
+
+</head>
+<body>
+
+
+
+
+ <div id="root"></div>
+</body>
+</html>
+
+
