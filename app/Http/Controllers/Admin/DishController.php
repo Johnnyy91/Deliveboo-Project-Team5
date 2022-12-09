a9cc0c759217c53cb2b1f473c26c3363ee7000ff
PietroDanieli <1pie7d@gmail.com>
2022-12-09 09:43:47 +0100

    add delete dish

--- app/Http/Controllers/Admin/DishController.php
+++ app/Http/Controllers/Admin/DishController.php
@@ -95,7 +95,8 @@ class DishController extends Controller
      */
     public function destroy(Dish $dish)
     {
-        //
+        $dish->delete();
+        return redirect()->route('admin.restaurants.index');
     }
 
 
--- resources/views/admin/dishes/index.blade.php
+++ resources/views/admin/dishes/index.blade.php
@@ -7,6 +7,11 @@
 
 
     Name:{{$item->name}} <span class="mx-5">Price:{{$item->price}}</span>
+    <form class="mt-3" method="POST" action="{{ route('admin.dishes.destroy', $item->id) }}">
+        @csrf
+        @method('DELETE')
+        <input class="btn btn-success mb-3"onclick="return confirm('Do you really want to delete this dish?')" type="submit" value="Delete">
+    </form>
 
 
 
