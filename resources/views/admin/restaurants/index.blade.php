00721ca8ebd871b8b8a968ee82f2d436dded7280
PietroDanieli <1pie7d@gmail.com>
2022-12-11 07:45:13 +0100

    ADD COMMENT

--- resources/views/admin/restaurants/index.blade.php
+++ resources/views/admin/restaurants/index.blade.php
@@ -3,6 +3,7 @@
 
 @section('content')
 
+{{-- CONTROLLER CREATE --}}
 <?php $val = false ?>
 @foreach ($restaurants as $item)
 @if ($item->user_id == $user->id)
@@ -10,10 +11,12 @@
 @endif
 @endforeach
 
+{{-- CREATE --}}
 @if (!$val)
-
 <form action="{{ route('admin.restaurants.store') }}" method="post">
     @csrf
+
+    {{-- NAME --}}
     <div>
         <label for="name">Name:</label>
         <input required maxlength="255" type="text" name="name" value="{{ old('name', '') }}">
@@ -23,6 +26,8 @@
             </div>
         @enderror
     </div>
+
+    {{-- P.IVA --}}
     <div>
         <label for="p.iva">P.iva:</label>
         <input required maxlength="255" type="text" name="piva" value="{{ old('piva', '') }}">
@@ -32,6 +37,8 @@
             </div>
         @enderror
     </div>
+
+    {{-- ADDRESS --}}
     <div>
         <label for="address">Address:</label>
         <input required maxlength="255" type="text" name="address" value="{{ old('address', '') }}">
@@ -41,6 +48,8 @@
             </div>
         @enderror
     </div>
+
+    {{-- TIME SLOTS --}}
     <div>
         {{-- OPEN LUNCH --}}
         <span>Lunch  </span>
@@ -72,29 +81,31 @@
         @enderror
         {{-- CLOSE DINNER --}}
         <label for="dinner_time_slot_close">Close:</label>
-        <input required maxlength="255" type="time" name="dinner_time_slot_close" value="{{ old('dinner_time_slot_close', '') }}">
-        @error('dinner_time_slot_close')
+            <input required maxlength="255" type="time" name="dinner_time_slot_close" value="{{ old('dinner_time_slot_close', '') }}">
+            @error('dinner_time_slot_close')
             <div class="my-2 bg-danger text-white">
                 {{ $message }}
             </div>
         @enderror
     </div>
+
+    {{-- IMAGE --}}
     <div>
         <label for="image">Image:</label>
         <input type="file" name="image" disabled>
     </div>
 
-
+    {{-- TYOPOLOGIES --}}
     <label for="typology_id">Typology:</label>
-    <select name="typologies[]" id="typology_id" multiple>
-    @foreach ($typologies as $typology )
-        <option   {{ in_array($typology->id, old('typology', [])) ? 'selected' : '' }}
-         value="{{$typology->id}}">{{$typology->name}}</option>
-    @endforeach
+        <select name="typologies[]" id="typology_id" multiple>
+            @foreach ($typologies as $typology )
+                <option   {{ in_array($typology->id, old('typology', [])) ? 'selected' : '' }}
+                value="{{$typology->id}}">{{$typology->name}}
+                </option>
+            @endforeach
     </select>
 
-
-
+    {{-- SUBMIT --}}
     <input type="submit" value="Create">
 </form>
 
