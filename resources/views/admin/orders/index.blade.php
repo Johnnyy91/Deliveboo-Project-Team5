a7f213c0a7f87f9cf5db92b206c8c6c879f26e65
PietroDanieli <1pie7d@gmail.com>
2022-12-12 12:06:03 +0100

    insert orders and dishes

--- resources/views/admin/orders/index.blade.php
+++ resources/views/admin/orders/index.blade.php
@@ -3,6 +3,14 @@
 
 
 @section('content')
-<div>{{$total_price}}</div>
-
+@foreach ($orders as $order)
+<div>
+    <h2>Address order: {{ $order->address_client }}</h2>
+@foreach ($order->dishes as $dish)
+{{-- QUI NOI VEDIAMO TUTTI I PIATTI ASSOCIATI A UN ORDINE --}}
+    Dishes:
+    {{ $dish->name }}
+@endforeach
+</div>
+@endforeach
 @endsection
