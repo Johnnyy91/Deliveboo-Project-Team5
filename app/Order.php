e2c92f984ce9d812c20032271282b206822f5d1f
PietroDanieli <1pie7d@gmail.com>
2022-12-12 07:08:49 +0100

    link to dish and protect fillable

--- app/Order.php
+++ app/Order.php
@@ -7,4 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
     //
+    public function dishes()  {
+        return $this->belongsToMany('App\Dish');
+    }
+
+    protected $fillable = [ 'address_client', 'email_client', 'date_order'];
+    //data_order verrà generato in automatico con un time() che restituirà l'ora corrente
 }
