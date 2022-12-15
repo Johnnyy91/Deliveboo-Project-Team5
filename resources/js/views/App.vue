48aa7031af4359a5cacbc10bb2cc428aafcf715e
ErosEros <eros.vittori1994@gmail.com>
2022-12-15 18:02:45 +0100

    add OrderComponent

--- resources/js/views/App.vue
+++ resources/js/views/App.vue
@@ -2,6 +2,7 @@
     <div>
     <h1>Deliveboo</h1>
    <HeaderComponent />
+   <OrderComponent />
    <router-view> </router-view>
 
     </div>
@@ -10,11 +11,11 @@
 
 <script>
 import HeaderComponent from '../components/HeaderComponent.vue'
+import OrderComponent from '../components/OrderComponent.vue';
 
 export default {
-    name:'App',
-    components: HeaderComponent,
-
+    name: "App",
+    components: { HeaderComponent, OrderComponent }
 }
 </script>
 
