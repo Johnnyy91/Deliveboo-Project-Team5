2c8dfaf897e4037b590f5631db7cbd8afa68c8b6
ErosEros <eros.vittori1994@gmail.com>
2022-12-15 17:13:54 +0100

    create order component with form

---
+++ resources/js/components/OrderComponent.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <form action="{{ route('admin.order.store') }}" method="post" enctype="multipart/form-data">
+
+            <!-- TOKEN CSRF -->
+            <input type="hidden" name="_token" v-bind:value="csrf">
+
+            <!-- EMAIL -->
+            <label for="email">Email Client</label>
+            <input type="email" id="email" name="email_client" >
+
+            <!-- ADDRESS -->
+            <label for="address">Address Client</label>
+            <input type="text" id="address" name="address_client">
+
+
+            <input type="submit" value="Send">
+
+
+        </form>
+    </div>
+</template>
+<script>
+export default {
+    name: 'OrderComponent',
+    data(){
+          return {
+            //csrf token
+             csrf: document.querySelector('meta[name="csrf-token"]').getAttribute('content'),
+       }
+    }
+
+}
+</script>
+<style lang="">
+
+</style>
