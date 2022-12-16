e6b401cbde02cbd670ee60a748976598acf69bf3
ErosEros <eros.vittori1994@gmail.com>
2022-12-16 19:12:25 +0100

    create "component not found"

---
+++ resources/js/components/notfound.vue
@@ -0,0 +1,30 @@
+<template>
+
+  <div class="py-5">
+
+    NOT FOUND
+
+  </div>
+
+</template>
+
+<script>
+
+        export default {
+        name: 'RestaurantDetails',
+        data() {
+            return {
+                restaurant: [],
+            }
+        },
+        }
+
+
+</script>
+
+<style lang="scss" scoped>
+
+@import '../../sass/app.scss';
+
+
+</style>
