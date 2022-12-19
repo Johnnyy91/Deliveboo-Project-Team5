fa583979a1aabec788882b347c7a52a5d549a85e
Johnnyy91 <claudiosardilli@gmail.com>
2022-12-19 13:03:19 +0100

    add submit , style and review form on OrderComponent

--- resources/js/components/OrderComponent.vue
+++ resources/js/components/OrderComponent.vue
@@ -1,6 +1,9 @@
 <template>
-    <div class="pt-5 text-center">
-        <form action="" method="post" enctype="multipart/form-data">
+    <div class="pt-5 text-center buyClass">
+    <div class="flex flex-row-reverse w-100">
+    <button @click="closeMod">X</button>
+    </div>
+        <form ref="order"  @submit.prevent="clicked()">
 
             <!-- TOKEN CSRF -->
             <!-- <input type="hidden" name="_token" v-bind:value="csrf"> -->
@@ -9,12 +12,14 @@
             <label for="email">Email Client</label>
             <input v-model="email" type="email" id="email" name="email_client" >
 
+
+
             <!-- ADDRESS -->
             <label for="address">Address Client</label>
             <input v-model="address" type="text" id="address" name="address_client">
 
 
-            <input type="button" @click="clicked()" value="Send">
+            <input type="submit" value="Send">
 
 
         </form>
@@ -32,16 +37,40 @@ export default {
             email:undefined
        }
     },
+    mounted() {
+        console.log(this.cart)
+    },
+    props: {
+        cart: Array,
+    },
 
     methods: {
         clicked(){
             console.log(this.email)
             console.log(this.address)
 
+
+        },
+        closeMod() {
+            this.$parent.$data.validation = false;
         }
     },
 }
 </script>
-<style lang="">
+<style>
+
+.buyClass{
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    width: 50%;
+    height: 50%;
+    background-color: #fff;
+    border-radius: 10px;
+    z-index: 999;
+
+
+}
 
 </style>
