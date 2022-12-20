39b68020988c720c9776e4743ac379a08f810ada
ErosEros <eros.vittori1994@gmail.com>
2022-12-20 11:07:17 +0100

    adjustments braintree controller

--- app/Http/Controllers/braintreeController.php
+++ app/Http/Controllers/braintreeController.php
@@ -1,103 +1,73 @@
 <?php
 
-namespace App\Http\Controllers\Dashboard;
+namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
-use App\User;
-use App\models\Sponsor;
 use Illuminate\Http\Request;
 use Braintree_Transaction;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
+use App\Restaurant;
 
 class BraintreeController extends Controller
 {
-    public function token(Request $request) {
+    public function createTransaction(Request $request) {
         // Create new Gateway
         $gateway = new \Braintree\Gateway([
-            'environment' => env('BRAINTREE_ENVIRONMENT'),
-            'merchantId' => env('BRAINTREE_MERCHANT_ID'),
-            'publicKey' => env('BRAINTREE_PUBLIC_KEY'),
-            'privateKey' => env('BRAINTREE_PRIVATE_KEY')
-        ]);
-
-        // Create Client Token
-        $clientToken = $gateway->clientToken()->generate();
-        $sponsor = Sponsor::find($request->sponsor);
-
-
-        // Return view containing form for payment
-        return view('dashboard.profile.braintree', compact('clientToken', 'gateway', 'sponsor'));
-    }
-
-    public function checkout(Request $request) {
-        // Create new Gateway
-        $gateway = new \Braintree\Gateway([
-            'environment' => env('BRAINTREE_ENVIRONMENT'),
-            'merchantId' => env('BRAINTREE_MERCHANT_ID'),
-            'publicKey' => env('BRAINTREE_PUBLIC_KEY'),
-            'privateKey' => env('BRAINTREE_PRIVATE_KEY')
+            'environment' => "sandbox",
+            'merchantId' => "nr7dbky87tmcnygt",
+            'publicKey' => "bwwzqch264jn9y2s",
+            'privateKey' => "e8e4b347eadbd6dd25628f6da1413cec"
         ]);
 
         // Find current user who is making the transaction
-        $user = User::find(Auth::id());
-
-        // Sponsor
-        $sponsor = Sponsor::find($request->sponsor);
-
-        // Get amout and nonce
-        $amount = $sponsor->price;
-        $nonce = $request->payment_method_nonce;
-
         // Create the transaction
         $result = $gateway->transaction()->sale([
-            'amount' => $amount,
-            'paymentMethodNonce' => $nonce,
-            'customer' => [
-                'firstName' => $user->name,
-                'lastName' => $user->lastname,
-                'email' => $user->email,
-                'id' => $user->id,
-            ],
+            'amount' => $request->amount,
+            'paymentMethodNonce' => $request->payload['nonce'],
             'options' => [
                 'submitForSettlement' => true
             ]
         ]);
 
-
         if ($result->success) {
-            // return 'Transaction Successful' . $result->transaction->id;
-            $sponsorRecord = DB::table('sponsor_user')->where( 'user_id','=', $user->id )->orderByDesc('created_at')->first();
-            if ( $sponsorRecord == null ||  $sponsorRecord->expire_date < date('Y-m-d H:i:s') ) {
-                //
-                $request->validate([
-                    'sponsor' => ['required', 'exists:sponsors,id']
-                ]);
+            // Transaction successful
+            // INSERIRE IN TABELLA L'ORDINE RICEVUTO
+            $restaurant = Restaurant::query()->where('slug', $request->slug)->first();
 
-                $sponsor = Sponsor::find($request->sponsor);
-                $newDate = date("Y-m-d H:i:s", strtotime("+{$sponsor->duration} hours"));
-                $user->sponsors()->attach($sponsor->id, ['expire_date' => $newDate, 'created_at' => date('Y-m-d H:i:s')]);
 
-                return redirect()->route('dashboard.profile.sponsor.store')->with('success','Sponsorizzazione aggiunta con successo!');
 
-                //
-            } elseif ( $sponsorRecord->expire_date > date('Y-m-d H:i:s') ) {
-            //
-                return redirect()->route('dashboard.profile.sponsor.store')->with('error','Sponsorizzazione ');
+            $order = $restaurant->orders()->create([
+                'total_price' => $request->amount,
+                'address_client' => $request->address,
+                'email_client' => $request->email,
+                'date_order' => date('Y-m-d H:i:s')
+            ]);
+
+            /**
+             * CART è UN ARRAY
+             * $cart = [
+             * 'id' => 1,
+             * 'name' => 'Pasta',
+             * 'price' => 10,
+             * 'quantity' => 2,
+             * 'restaurant_id' => 1,
+             *
+             * ]
+             */
+
+            $cart = $request->cart;
+
+            foreach($cart as $dish) {
+                $order->dishes()->attach($dish->id);
             }
+
+            return 'Transaction Successful' . $result->transaction->id;
+
+
         }
         else {
-            $errorString = "";
+            return 'Transaction Failed';
 
-            foreach ($result->errors->deepAll() as $error) {
-                $errorString .= 'Error: ' . $error->code . ": " . $error->message . "\n";
-            }
-            return 'An error occurred with the message: '.$result->message;
         }
     }
-
-    public function transactionDetails() {
-        //
-    }
 }
-
