c40288b856879f014fd8c89a1476bae209c6bc81
ErosEros <eros.vittori1994@gmail.com>
2022-12-20 02:42:21 +0100

    add script braintree

--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="{{ asset('css/app.css')}}">
     <script src="{{ asset('js/front.js')}}" defer></script>
+    <script src="https://js.braintreegateway.com/web/dropin/1.22.1/js/dropin.min.js"></script>
     <title>DeliveBoo</title>
 
 </head>
