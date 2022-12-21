5327204d76218a6741b56c99c704829c21439b53
ErosEros <eros.vittori1994@gmail.com>
2022-12-21 22:25:32 +0100

    hover for logo to return in home

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,10 +24,11 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand logo" href="{{ url('/') }}">
-                    <img src="https://s3-eu-west-1.amazonaws.com/tpd/logos/55be6ade0000ff000581b457/0x0.png" alt="logo" class="fl-logo d-flex">
-                    <span>AREA RISERVATA</span>
-
+                    <img src="https://s3-eu-west-1.amazonaws.com/tpd/logos/55be6ade0000ff000581b457/0x0.png" alt="logo" class="logo-img fl-logo d-flex">
+                    <span class="backHome">Torna in Homepage</span>
                 </a>
+
+
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
@@ -37,10 +38,11 @@
                     <ul class="navbar-nav mr-auto">
 
                     </ul>
-
+                    <span>AREA RISERVATA</span>
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ml-auto">
                         <!-- Authentication Links -->
+
                         @guest
                             <li class="nav-item">
                                 <a class="nav-link" href="{{ route('login') }}">{{ __('Accedi') }}</a>
@@ -85,10 +87,30 @@
     .logo {
         display: flex;
         align-items: center;
+
+    }
+
+    .logo-img:hover + .backHome{
+        display: block;
+        color: white;
+        background-color: black;
+        padding: 0.2%;
+
+
     }
 
     img {
         width: 50px;
         padding-right: 10px;
+    }
+
+    .backHome {
+        display: none;
+        position: absolute;
+        top: 45%;
+        left: 17%;
+        font-size: 0.7rem;
+
+
     }
 </style>
