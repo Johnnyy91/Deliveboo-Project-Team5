6363f9193ee14095b854ffa8689915410df4b29e
ErosEros <eros.vittori1994@gmail.com>
2022-12-22 00:47:45 +0100

    card style

--- resources/js/components/RestaurantDetails.vue
+++ resources/js/components/RestaurantDetails.vue
@@ -1,30 +1,52 @@
 <template>
 
-    <div class="py-5">
-        <div class="py-5 card" v-for="restaurant in restaurants" :key="restaurant.id">
-            <h1>{{ restaurant.name }}</h1>
-            <img :src="'/storage/' + restaurant.img" alt="">
-            <h1>Indirizzo:</h1>
-            <h2>{{ restaurant.address }}</h2>
-            <div class="card-time d-flex justify-content-between text-center">
-                <div>
-                    <h3>Orario Pranzo:</h3>
-                    <h4>{{ restaurant.lunch_time_slot_open }}</h4>
-                    <h4>{{ restaurant.lunch_time_slot_close }}</h4>
-                </div>
-                <div>
-                    <h3>Orario Cena:</h3>
-                    <h4>{{ restaurant.dinner_time_slot_open }}</h4>
-                    <h4>{{ restaurant.dinner_time_slot_close }}</h4>
+    <div class="py-5 container-first">
+        <div class="row d-flex">
+            <div class=" col-lg-12 col-md-4">
+                <div class="py-5 card-restaurant d-flex" v-for="restaurant in restaurants" :key="restaurant.id">
+                    <div>
+                        <img :src="'/storage/' + restaurant.img" alt="">
+                    </div>
+
+
+                    <div>
+
+                        <div>
+                            <span>Nome Ristorante:</span>
+                            <h3 class="d-inline">{{ restaurant.name }}</h3>
+                        </div>
+
+                        <div>
+                            <span>Indirizzo:</span>
+                       <h3 class="d-inline">{{ restaurant.address }}</h3>
+                        </div>
+
+
+
+                            <div class="">
+                                <div>
+                                    <h5 class="d-inline">Orario Pranzo:</h5>
+                                    <span>{{ restaurant.lunch_time_slot_open }}</span>
+                                    <span>{{ restaurant.lunch_time_slot_close }}</span>
+                                </div>
+                                <div>
+                                    <h5 class="d-inline">Orario Cena:</h5>
+                                    <span >{{ restaurant.dinner_time_slot_open }}</span>
+                                    <span>{{ restaurant.dinner_time_slot_close }}</span>
+                                </div>
+                            </div>
+                                <div>
+                                    <span v-for="typology in restaurant.typologies" :key="typology.id">Tipologia:{{ typology.name }}</span>
+
+                                    <router-link :to="'/menu/' + restaurant.slug" class="btn btn-danger d-block">Visualizza Menù</router-link>
+                                </div>
+
+
+                    </div>
+
                 </div>
             </div>
-            <div><span v-for="typology in restaurant.typologies" :key="typology.id">{{ typology.name }}</span></div>
-
-            <router-link :to="'/menu/' + restaurant.slug" class="btn btn-danger mt-5">Visualizza Menù</router-link>
         </div>
-
-
-
     </div>
 
 </template>
@@ -61,12 +83,24 @@ export default {
 
 
 img {
-    width: 100%;
+    width: 20vw;
+    height: 25vh;
+    border-radius: 1rem;
 }
 
-.card {
-    width: 500px;
-    background-color: #f6f6f6;
-    margin: 20px 20px;
+.card-restaurant {
+    width: 50vw;
+    background: radial-gradient(circle at -8.9% 51.2%, rgb(4, 202, 187) 0%, rgb(4, 202, 187) 15.9%, #009688 15.9%, #009688 24.4%, rgb(46 51 51) 24.5%, rgb(46 51 51) 66%);
+    margin: 50px auto;
+    // border: 2px solid black;
+    justify-content: space-around;
+    border-radius: 1rem;
+    color: white
 }
+
+span{
+    font-size: 1rem
+}
+
+
 </style>
