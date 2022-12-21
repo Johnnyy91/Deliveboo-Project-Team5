117fd81906a5b2d3a9bdd4471ba945167782252a
PietroDanieli <1pie7d@gmail.com>
2022-12-21 11:55:04 +0100

    I implement the function that was intended for the braitree controller in the routes\api

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Order;
+use App\Restaurant;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,6 +19,62 @@ use Illuminate\Support\Facades\Route;
 
 Route::resource('restaurant', 'api\FrontendController'); // abilito il controller api
 Route::get('menu/{slug}', 'api\DishController@show');
-Route::post('process-payment', 'api\BraintreeController@createTransaction');
+Route::post('process-payment', function (Request $request) {
+
+    $gateway = new \Braintree\Gateway([
+        'environment' => "sandbox",
+        'merchantId' => "nr7dbky87tmcnygt",
+        'publicKey' => "bwwzqch264jn9y2s",
+        'privateKey' => "e8e4b347eadbd6dd25628f6da1413cec"
+    ]);
+
+    // Find current user who is making the transaction
+    // Create the transaction
+    $result = $gateway->transaction()->sale([
+
+        'amount' => $request->amount,
+        'paymentMethodNonce' => $request->payload['nonce'],
+        'options' => [
+            'submitForSettlement' => true
+        ]
+    ]);
+
+    if ($result->success) {
+
+
+        // $restaurant = Restaurant::query()->where('slug', $request->restaurant)->first();
+        $restaurant = Restaurant::find($request->cart[0]['restaurant_id']);
+
+        $order = Order::create([
+            'total_price' => $request->amount,
+            'address_client' => $request->address,
+            'email_client' => $request->email,
+            'date_order' => date('Y-m-d H:i:s'),
+            'restaurant_id'=> $restaurant->id
+        ]);
+
+        foreach($request->cart as $dish) {
+            if($dish['count'] > 1 ) {
+                for($i = 0; $i<$dish['count']; $i++) {
+                    $order->dishes()->attach($dish['id']);
+                }
+            }
+            else {
+                $order->dishes()->attach($dish['id']);
+            }
+        }
+
+        return 'Transaction Successful' . $result->transaction->id;
+
+
+    }
+    else {
+
+
+        return 'Transaction Failed';
+
+    }
+});
+
 //Route::get('/restaurants/{id}','api\FrontendController@show');
 Route::get('test', function(){return response('ciao');});
