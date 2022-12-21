1e09849a1afc8faf04ebb70e85fd6f4234fdbeff
PietroDanieli <1pie7d@gmail.com>
2022-12-21 11:52:02 +0100

    connect Models: Order to Restaurant

--- app/Order.php
+++ app/Order.php
@@ -1,7 +1,8 @@
 <?php
 
 namespace App;
-
+use App\Dish;
+use App\Restaurant;
 use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
@@ -10,7 +11,10 @@ class Order extends Model
     public function dishes()  {
         return $this->belongsToMany('App\Dish');
     }
+    public function restaurant()  {
+        return $this->belongsTo('App\Restaurant');
+    }
 
-    protected $fillable = [ 'address_client', 'email_client', 'date_order'];
+    protected $fillable = [ 'total_price','address_client', 'email_client', 'date_order','restaurant_id'];
     //data_order verrà generato in automatico con un time() che restituirà l'ora corrente
 }
