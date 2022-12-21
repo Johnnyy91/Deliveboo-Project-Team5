75cdb6dfda29fed419ff1396876401f96f6ef64d
PietroDanieli <1pie7d@gmail.com>
2022-12-21 11:56:16 +0100

    new query for OrderController@index

--- app/Http/Controllers/Admin/OrderController.php
+++ app/Http/Controllers/Admin/OrderController.php
@@ -22,20 +22,46 @@ class OrderController extends Controller
     {
         //
 
+        // $user = Auth::user();
+        // $restaurant = Restaurant::where('user_id', $user->id)->first();
+        // $dishes = Dish::where('restaurant_id', $restaurant->id)->get();
+        // //temporany
+        // $query  = DB::table('users')
+        // ->join('restaurants', 'users.id', '=', 'restaurants.user_id')
+        // ->join('dishes', 'restaurants.id', '=', 'dishes.restaurant_id')
+        // ->join('dish_order', 'dishes.id', '=', 'dish_order.dish_id')
+        // ->join('orders', 'orders.id', '=', 'dish_order.order_id')
+        // ->select('orders.*')
+        // ->where('user_id', $user->id)
+        // ->groupBy('orders.id')
+        // ->get();
+        // return view('admin.orders.index', compact('query'));
         $user = Auth::user();
-        $restaurant = Restaurant::where('user_id', $user->id)->first();
-        $dishes = Dish::where('restaurant_id', $restaurant->id)->get();
-        //temporany
-        $query  = DB::table('users')
-        ->join('restaurants', 'users.id', '=', 'restaurants.user_id')
-        ->join('dishes', 'restaurants.id', '=', 'dishes.restaurant_id')
-        ->join('dish_order', 'dishes.id', '=', 'dish_order.dish_id')
-        ->join('orders', 'orders.id', '=', 'dish_order.order_id')
-        ->select('orders.*')
-        ->where('user_id', $user->id)
-        ->groupBy('orders.id')
-        ->get();
-        return view('admin.orders.index', compact('query'));
+        $restaurant = Restaurant::where('user_id', $user->id)->with('dishes')->first();
+        $orders = Order::where('restaurant_id', $restaurant->id)->with('dishes')->orderByDesc('id')->get();
+
+        $groupedOrders = $orders->map(function($order) {
+            $dishes = $order->dishes->groupBy('id')->map(function($dishGroup) {
+                return [
+                    'id' => $dishGroup->first()->id,
+                    'name' => $dishGroup->first()->name,
+                    'quantity' => $dishGroup->count()
+                ];
+            });
+
+            return [
+                'id' => $order->id,
+                'address_client' => $order->address_client,
+                'total_price' => $order->total_price,
+                'created_at' => $order->created_at,
+                'email_client' => $order->email_client,
+                'dishes' => $dishes
+            ];
+        });
+
+
+
+        return view('admin.orders.index', compact('groupedOrders'));
     }
 
     /**
