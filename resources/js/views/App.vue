906ebeffdc7fa8450d25e79becf6432381df5718
Johnnyy91 <claudiosardilli@gmail.com>
2022-12-21 16:56:15 +0100

    modification AppVue

--- resources/js/views/App.vue
+++ resources/js/views/App.vue
@@ -1,35 +1,28 @@
 <template>
-
-    <main>
+    <div id="root">
         <HeaderComponent />
-        <router-view class="space-from-header px-4"> </router-view>
-
-
-    </main>
-
+         <main>
+            <router-view class="space-from-header px-4"> </router-view>
+         </main>
+         <FooterComponent />
+    </div>
 </template>
 
 <script>
 
-    import HeaderComponent from '../components/HeaderComponent.vue';
-    import HeroComponent from '../components/HeroComponent.vue';
 
-    import RestaurantDetails from '../components/RestaurantDetails.vue';
-    import WorkWithUsComponent from '../components/WorkWithUsComponent.vue'
+    import HeaderComponent from '../components/HeaderComponent.vue';
+    import FooterComponent from '../components/FooterComponent.vue';
+
 
 
     export default {
 
-        name:'App',
-        components: {
-            HeaderComponent,
-            HeroComponent,
-
-            RestaurantDetails,
-            WorkWithUsComponent,
-
-
-        }
+    name:'App',
+    components: {
+       HeaderComponent,
+     FooterComponent
+}
 
     }
 
